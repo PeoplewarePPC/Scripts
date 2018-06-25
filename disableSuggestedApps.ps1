c3deaa69b8c53e0481222f7bf2f3474c0822346c
PeoplewarePPC <40198810+PeoplewarePPC@users.noreply.github.com>
2018-06-25 13:00:32 +0200

    Add files via upload

---
+++ disableSuggestedApps.ps1
@@ -0,0 +1,9 @@
+﻿$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
+$Name = "DisableWindowsConsumerFeatures"
+$value = "1"
+if(!(Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+}else {
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+}
